0b4d48695f093f5d7c2b187fdc412f316f62b646
Avantika Ajit <avantika.aji22@gmail.com>
2024-10-20 09:25:04 +0530

    update

---
+++ FAS.py
@@ -0,0 +1,12 @@
+#Importing necessary libraries
+import face_recognition
+import cv2
+import numpy as np
+import os
+import pandas as pd
+from datetime import datetime
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+from PIL import Image, ImageTk
+
